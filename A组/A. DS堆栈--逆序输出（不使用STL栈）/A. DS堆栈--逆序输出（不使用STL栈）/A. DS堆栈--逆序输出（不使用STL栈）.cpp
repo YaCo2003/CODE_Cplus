2245eb74d3d40c3f2993ed8679ccd4adf9d2296d
ye-zhizhi <3020936216@qq.com>
2023-11-02 23:36:34 +0800

    A. DS堆栈--逆序输出（不使用STL栈）

---
+++ A组/A. DS堆栈--逆序输出（不使用STL栈）/A. DS堆栈--逆序输出（不使用STL栈）/A. DS堆栈--逆序输出（不使用STL栈）.cpp
@@ -0,0 +1,23 @@
+﻿// A. DS堆栈--逆序输出（不使用STL栈）.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int t = 0;
+	cin >> t;
+	while (t--)
+	{
+		string str;
+		int len;
+		cin >> str; //把输入的字符串保存在变量str;
+		len = str.length();  //获取输入字符串的长度
+		for (int i = len - 1; i >= 0; i--)
+		{
+			cout << str[i];
+		}
+		cout << endl;
+	}
+	return 0;
+}
